6c1ad10cbca9995be0cc0f1f0c3bd331c2500e64
chaeyeon-yang <myday0827@naver.com>
2024-04-02 14:58:58 +0900

    study: mybatis-insert 예외처리

--- sb2/src/test/java/com/hana/cust/InsertTests.java
+++ sb2/src/test/java/com/hana/cust/InsertTests.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DuplicateKeyException;
+
+import java.sql.SQLException;
 
 @SpringBootTest
 @Slf4j
@@ -15,13 +18,18 @@ class InsertTests {
     CustService custService;
     @Test
     void contextLoads() {
-        CustDto custDto = CustDto.builder().id("id77").pwd("pwd77").name("이말숙").build();
+        CustDto custDto = CustDto.builder().id("id88").pwd("pwd88").name("강영현").build();
         try {
             custService.add(custDto);
             log.info("----------OK----------------");
         } catch (Exception e) {
-            log.info("----------Fail----------------");
-            e.printStackTrace();
+            if(e instanceof SQLException){
+                log.info("----------System Trouble EX0001----------------");
+            }else if(e instanceof DuplicateKeyException){
+                log.info("----------ID Duplicated EX0002----------------");
+            }else{
+                log.info("----------Sorry EX0003----------------");
+            }
         }
     }
 
